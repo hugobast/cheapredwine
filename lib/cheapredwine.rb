1e7954590e7f66d68357da8c77c407959b885aae
Hugo Bastien <hugobast@gmail.com>
2013-12-11 20:19:14 -0500

    added a way to build an image and not execute the command

--- lib/cheapredwine.rb
+++ lib/cheapredwine.rb
@@ -5,26 +5,30 @@ class Cheapredwine
   extend Forwardable
 
   attr_accessor :file
-  def_delegators :@font_attrs, :font_name, :family_name, :style, :features
+  def_delegators :@attrs, :font_name, :family_name, :style, :features
 
   def initialize file
-    @font_attrs = Parser.new file
+    @attrs = Parser.new file
     @file = File.new file
   end
 
   def image &block
-    builder = Cheapredwine::Command::Builder.new
+    create(&block).run!
+  end
+
+  def create &block
     builder.instance_eval &block
     builder.font_file file.path
-    Cheapredwine::Command::Runner.new(builder).run!
+    runner
   end
 
   private
 
-  def merge_features(all_features, selected_features)
-    all_features.map do |feature|
-      prefix = selected_features.include?(feature) ? '+' : '-'
-      prefix + feature
-    end
+  def builder
+    @builder ||= Command::Builder.new
+  end
+
+  def runner
+    @runner ||= Command::Runner.new builder
   end
 end
